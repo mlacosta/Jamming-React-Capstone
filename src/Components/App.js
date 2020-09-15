e95583f9503ae3f5d74d45bc40bb37afebbc5b12
mlacosta <marianoacosta.003@gmail.com>
2020-09-15 15:14:20 -0300

    linking react components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchResults from './SearchResults/SearchResult';
+import SearchBar from './SearchBar/SearchBar';
+import Playlist from './Playlist/Playlist';
+let someTracks  = [
+                    {name:'Automata', artist:'She', album:'Chroma',id:1},
+                    {name:'Interestellar', artist:'Au5', album:'Interestellar',id:2},
+                    {name:'Always Yours', artist:'She', album:'Unknown',id:3},
+                    {name:'C Stick', artist:'game Boi Adv', album:'Unknown',id:4},
+                  ]
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <div className="App-playlist">
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {SearchResults:[{name:'Bangarang', artist:'Skrillex', album:'Bangarang',id:01}],
+                  playlistName: 'Awesome',
+                  playlistTrack: someTracks};
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults = {this.state.SearchResults}/>
+            <Playlist playlistName = {this.state.playlistName} playlistTrack = {this.state.playlistTrack}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
+
 }
 
 export default App;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Tracklist.css';
+import Track from '../Track/Track';
 
 export class Tracklist extends React.Component{
 
     render(){
         return (
             <div className="TrackList">
-                
+                {this.props.tracks.map(
+                    (track) =>{
+                        <Track track={track}/>
+                    })
+                }
             </div>
         )
     }
+
 }
